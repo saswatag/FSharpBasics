096366e696a5d05790457b82727756fdf4d03008
Sas <saswata.goswami@flarehr.com>
2022-01-24 09:54:57 +1100

    Update Basics.fsx
    Changed to a more functional way of driving the number printer

--- FSharpBasics/FSharpBasics/Basics.fsx
+++ FSharpBasics/FSharpBasics/Basics.fsx
@@ -1,5 +1,116 @@
-﻿// allow creating only valid numbers (validity criteria here: [1,4000])
-// later change the validity criteria
+﻿let result =
+    let i, j, k = (1, 2, 3)
+    // Body expression:
+    i + 2*j + 3*k
+
+
+let resultNew =
+    let function3 (a, b) = a + b
+    100 * function3 (1, 2)
+
+
+///////////////////////////////////////////////////
+
+let cylinderVolume (radius : int) (length : int) =
+    
+    let pi = 3.14159
+    (float length) * pi * (float radius) * (float radius)
+
+cylinderVolume 10 10
+
+/////////////////////////////////////
+
+let apply (transform : int -> int -> int ) y = transform y
+
+let result3 = apply (fun x y -> x * y) 2 3
+
+/////////////////////////////////////
+
+let seq1 = seq { for i in 1 .. 10 -> (i, i*i) }
+printfn $"List is: {seq1}"
+
+////////////////////////////////////
+
+let add3 number = 3. + number
+
+let times2 number = 2. * number
+
+let pow2 number = number ** 2.0
+
+let operation number = 
+    number
+    |> add3
+    |> times2
+    |> pow2
+
+operation 2
+
+/////////////////////////
+
+type PaymentType = 
+    | BankAccount
+    | Card
+
+type BankAccountDetails =
+    {
+        BSB: string
+        AccountNumber: string
+        BankName: string
+    }
+
+type BankContactResponse =
+    {
+        Id: string
+        Type: PaymentType
+        ContactDetails: BankAccountDetails
+    }
+
+let dummyBankContactResponse = 
+    [{ Id = "dummy id string 1"; Type = PaymentType.BankAccount; ContactDetails = { BSB = "222"; AccountNumber = "12345"; BankName = "ANZ" } };
+     { Id = "dummy id string 2"; Type = PaymentType.BankAccount; ContactDetails = { BSB = "333"; AccountNumber = "6789"; BankName = "ING" } }]
+
+//////////////////////////////
+
+let add2 x = x + 2
+let mult3 x = x * 3
+let square x = x * x
+
+let logMsgN msg x = printfn "%s%i" msg x; x
+
+logMsgN "some number:" 10
+
+let listOfFunctions = [
+   mult3;
+   square;
+   add2;
+   logMsgN "result=";
+   ]
+
+// compose all functions in the list into a single one
+let allFunctions = List.reduce (>>) listOfFunctions
+
+//test
+allFunctions 5
+
+//////////////////////////////////////////////
+
+let fizzBuzz listOfNumbers = listOfNumbers
+                                |> List.map (fun n -> (n, n%3, n%5))
+                                |> List.map (function
+                                    | (_,0,0) -> "FizzBuzz"
+                                    | (_,0,_) -> "Fizz"
+                                    | (_,_,0) -> "Buzz"
+                                    | (n,_,_) -> string n + " Not a factor of 3 or 5")
+
+let numbers = [1 .. 500]
+
+fizzBuzz numbers
+
+/////////////////////////////////////////
+
+
+// allow creating only valid numbers (validity criteria here: [1,4000])
+// next diplay validation error messages
 
 module NumberWithin1To4000 = 
     type ValidNumber = private ValidNumber of int
@@ -12,12 +123,9 @@ module NumberWithin1To4000 =
         | Within1To4000 -> Some (ValidNumber number)
         | NotInRange -> None
 
-    let numberValue (ValidNumber number) = number
+    let numberValue (ValidNumber number) = number    
 
-    let displayMessage (number: ValidNumber option) = 
-        match number with
-        | Some number -> $"The number {numberValue number} is in the range [1,4000]"
-        | None -> $"The number passed in is not in the range [1,4000]"
+    let displayMessage (number: ValidNumber) = $"The number {numberValue number} is within range [1,4000]"
 
 
 open NumberWithin1To4000
@@ -25,8 +133,12 @@ let invalidNumber = NumberWithin1To4000.tryCreateValidNumber -1
 let invalidNumber1 = NumberWithin1To4000.tryCreateValidNumber 5000
 let validNumber = NumberWithin1To4000.tryCreateValidNumber 2000
 
-let numberPrinter validNumber =
-    NumberWithin1To4000.displayMessage validNumber
+let numberPrinter (number: ValidNumber option) =
+    match number with
+    | Some number -> NumberWithin1To4000.displayMessage number
+    | None -> $"The number passed in is not in range"
 
-numberPrinter invalidNumber
-numberPrinter validNumber
+
+[-1; 1; 2000; 4000; 5000]
+    |> List.map (fun n -> NumberWithin1To4000.tryCreateValidNumber n)
+    |> List.map (fun n -> numberPrinter n)

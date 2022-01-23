6b76af63eabaca7f00e1670d7e37df3967d0f1e6
Sas <saswata66@gmail.com>
2022-01-23 23:09:36 +1100

    Changes

--- FSharpBasics/FSharpBasics/Basics.fsx
+++ FSharpBasics/FSharpBasics/Basics.fsx
@@ -7,15 +7,12 @@ module NumberWithin1To4000 =
     let (|Within1To4000|NotInRange|) (number: int) = 
         if 1 <= number && number <= 4000 then Within1To4000 else NotInRange
 
-    let createValidNumber (number:int) = 
+    let tryCreateValidNumber (number:int) = 
         match number with 
         | Within1To4000 -> Some (ValidNumber number)
         | NotInRange -> None
 
-    let numberValue (number: ValidNumber option) = 
-        match number with
-        | Some number -> number
-        | None -> string ""
+    let numberValue (ValidNumber number) = number
 
     let displayMessage (number: ValidNumber option) = 
         match number with
@@ -24,12 +21,12 @@ module NumberWithin1To4000 =
 
 
 open NumberWithin1To4000
-let invalidNumber = NumberWithin1To4000.createValidNumber -1
-let invalidNumber1 = NumberWithin1To4000.createValidNumber 5000
-let validNumber = NumberWithin1To4000.createValidNumber 2000
+let invalidNumber = NumberWithin1To4000.tryCreateValidNumber -1
+let invalidNumber1 = NumberWithin1To4000.tryCreateValidNumber 5000
+let validNumber = NumberWithin1To4000.tryCreateValidNumber 2000
 
-let numberPrinter number =
-    NumberWithin1To4000.displayMessage number
+let numberPrinter validNumber =
+    NumberWithin1To4000.displayMessage validNumber
 
 numberPrinter invalidNumber
 numberPrinter validNumber

d4563c86cb24e462c8a3eff6cf416f8922b8a0d1
Sas <saswata66@gmail.com>
2022-02-01 23:27:02 +1100

    Auto stash before merge of "main" and "origin/main"

--- FSharpBasics/FSharpBasics/IntegerParser.fs
+++ FSharpBasics/FSharpBasics/IntegerParser.fs
@@ -54,6 +54,9 @@ module StringAddPipeIntoWorkflow =
     let log message = 
         printfn message
 
+    let sum number1 number2 = 
+        number1 + number2
+
     let pipeInto (stringValue, onSuccessfullParseInteger) = 
         let intMayBe = stringValue |> tryParseIntegerFromString
         match intMayBe with
@@ -61,41 +64,47 @@ module StringAddPipeIntoWorkflow =
             log $"{stringValue} is not and integer. Aborting add workflow"
             None
         | Some integer -> 
-            log $"{stringValue} is an integer. Proceeding with add workflow"
+            log $"{integer} is an integer. Proceeding with add workflow"
+            integer |> onSuccessfullParseInteger
 
 
     let stringAddWorkflow string1 string2 string3 string4 = 
-        let int1MayBe = string1 |> tryParseIntegerFromString
-        match int1MayBe with
-        | None -> 
-            log $"{string1} is not and integer. Aborting add workflow"
-            None
-        | Some int1 ->
+        let number1 = string1 |> tryParseIntegerFromString
 
-            log $"{string1} is an integer. Proceeding with add workflow"    
-            let int2MayBe = string2 |> tryParseIntegerFromString
-            match int2MayBe with
-            | None -> 
-                log $"{string2} is not and integer. Aborting add workflow"
-                None
-            | Some int2 ->
+        //pipeInto(string1,  |> sum string2)
 
-                log $"{string2} is an integer. Proceeding with add workflow"  
-                let int3MayBe = string3 |> tryParseIntegerFromString
-                match int3MayBe with
-                | None -> 
-                    log $"{string3} is not and integer. Aborting add workflow"
-                    None
-                | Some int3 ->
+    //let stringAddWorkflow string1 string2 string3 string4 = 
+    //    let int1MayBe = string1 |> tryParseIntegerFromString
+    //    match int1MayBe with
+    //    | None -> 
+    //        log $"{string1} is not and integer. Aborting add workflow"
+    //        None
+    //    | Some int1 ->
 
-                    log $"{string3} is an integer. Proceeding with add workflow"  
-                    let int4MayBe = string4 |> tryParseIntegerFromString
-                    match int4MayBe with
-                    | None -> 
-                        log $"{string4} is not and integer. Aborting add workflow"
-                        None
-                    | Some int4 ->
-                        log $"{string4} is an integer. Proceeding with add workflow"  
-                        Some ( int1 + int2 + int3 + int4 )
+    //        log $"{string1} is an integer. Proceeding with add workflow"    
+    //        let int2MayBe = string2 |> tryParseIntegerFromString
+    //        match int2MayBe with
+    //        | None -> 
+    //            log $"{string2} is not and integer. Aborting add workflow"
+    //            None
+    //        | Some int2 ->
+
+    //            log $"{string2} is an integer. Proceeding with add workflow"  
+    //            let int3MayBe = string3 |> tryParseIntegerFromString
+    //            match int3MayBe with
+    //            | None -> 
+    //                log $"{string3} is not and integer. Aborting add workflow"
+    //                None
+    //            | Some int3 ->
+
+    //                log $"{string3} is an integer. Proceeding with add workflow"  
+    //                let int4MayBe = string4 |> tryParseIntegerFromString
+    //                match int4MayBe with
+    //                | None -> 
+    //                    log $"{string4} is not and integer. Aborting add workflow"
+    //                    None
+    //                | Some int4 ->
+    //                    log $"{string4} is an integer. Proceeding with add workflow"  
+    //                    Some ( int1 + int2 + int3 + int4 )
 
     
